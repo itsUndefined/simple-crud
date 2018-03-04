75cbcb91bd20c441d047be28b85a810653c78d34
Θεμιστοκλής Χατζηεμμανουήλ <themis.chatzie@gmail.com>
2018-03-04 22:35:25 +0200

    Configured enviroment

---
+++ src-backend/main.ts
@@ -0,0 +1,18 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+
+let win: BrowserWindow;
+
+app.on('ready', () => {
+  win = new BrowserWindow({
+    x: 0,
+    y: 0,
+    width: 800,
+    height: 800
+  });
+
+  win.loadURL('http://localhost:4200');
+
+});
+
+
+
